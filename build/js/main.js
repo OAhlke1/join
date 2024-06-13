f8e8b8f5bc4214fdc7c64b23db948ec71cba38e4
Offermanns <p.offermanns@D-Bogdol.local>
2024-06-13 08:53:59 +0200

    New folders

--- build/js/main.js
+++ build/js/main.js
@@ -1,8 +1,10 @@
+
+
 async function includeHTML() {
     let includeElements = document.querySelectorAll('[w3-include-html]');
     for (let i = 0; i < includeElements.length; i++) {
         const element = includeElements[i];
-        let file = element.getAttribute("w3-include-html"); // "includes/nav.html"
+        let file = element.getAttribute("w3-include-html");
         let resp = await fetch(file);
         if (resp.ok) {
             element.innerHTML = await resp.text();
