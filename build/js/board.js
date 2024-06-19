b2cbb58336ec3e6432bbf2a01bd791df8a4c3bdc
OAhlke1 <oahlke1@gmail.com>
2024-06-19 19:12:41 +0200

    %*

--- build/js/board.js
+++ build/js/board.js
@@ -1,8 +1,31 @@
+let columnCardConts = document.querySelectorAll('.column-card-cont');
+
 function shiftParticipants() {
     document.querySelectorAll('main .boardCont .board .board-column .column-card-cont .user-story').forEach((elem, index) => {
         elem.querySelectorAll(' .participants-and-urgency .participants .participant').forEach((el, i)=>{
             el.style.left = `${-7*i}px`;
         })
     })
+    setEventListeners();
 }
-shiftParticipants();
+//shiftParticipants();
+
+let appendC = function appendCard(event) {
+    
+    event.target.append()
+}
+
+function setEventListeners() {
+    columnCardConts.forEach((elem)=>{
+        elem.addEventListener('drop');
+    })
+}
+
+function tiltCard(event) {
+    event.stopPropagation();
+    event.target.style.display = "none";
+}
+
+function tiltCardBack(event) {
+    
+}
