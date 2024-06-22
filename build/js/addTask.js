3a78b77b9777007e40be24ad3378dbc46e11c0f7
sebastian_iwasieczko@web.de <you@example.com>
2024-06-22 20:38:56 +0200

    addtask-first try

---
+++ build/js/addTask.js
@@ -0,0 +1,18 @@
+function addTaskFunction(event) {
+    event.preventDefault();
+    console.log(event);
+    document.getElementById("title-input").innerHTML;
+    document.getElementById("textarea-input").innerHTML;
+    document.getElementById("select-name").innerHTML;
+    document.getElementById("date-input").innerHTML;
+    document.querySelector(".clickedPrio");
+    document.getElementById("title-input").innerHTML;
+}
+
+function clickUrgency(event) {
+    console.log(event);
+    for (let i = 0; i < document.querySelectorAll(".choose-prio-button").length; i++) {
+        document.querySelectorAll(".choose-prio-button")[i].classList.remove("clickedPrio");
+    }
+    event.target.classList.add("clickedPrio");
+}
