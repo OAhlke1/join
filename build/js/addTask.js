560dac268f5b64d24db29ff422aa92d9f9cf16dc
sebastian_iwasieczko@web.de <you@example.com>
2024-06-26 00:48:37 +0200

    edit buttons

--- build/js/addTask.js
+++ build/js/addTask.js
@@ -1,22 +1,36 @@
-function addTaskFunction(event) {
+let tasks = [];
+let participants = [];
+let name, sureName, lastName;
+let selectedPrio = "";
+
+function addTask(event) {
     event.preventDefault();
-    console.log(event);
-    document.getElementById("title-input").innerHTML;
-    document.getElementById("textarea-input").innerHTML;
-    document.getElementById("select-name").innerHTML;
-    document.getElementById("date-input").innerHTML;
-    document.querySelector(".clickedPrio");
-    document.getElementById("title-input").innerHTML;
+    tasks.push({
+        title: document.getElementById("title-input").value,
+        storyDescrip: document.getElementById("textarea-input").value,
+        name: document.getElementById("selected-name").value,
+        date: document.getElementById("date-input").value,
+        urgency: selectedPrio,
+        subtask: document.getElementById("choose-subtasks").value,
+    });
+    console.log(tasks);
 }
 
-function clickUrgency(event = 0) {
-    if(event === 0) {
+function choosePrio(prio, buttonId) {
+    document.getElementById("prio-urgent-button").classList.remove("prio-urgent-button-bg-color");
+    document.getElementById("prio-medium-button").classList.remove("prio-medium-button-bg-color");
+    document.getElementById("prio-low-button").classList.remove("prio-low-button-bg-color");
 
-    }
-    event.stopPropagation();
-    console.log(event.target);
-    for (let i = 0; i < document.querySelectorAll(".choose-prio-button").length; i++) {
-        document.querySelectorAll(".choose-prio-button")[i].classList.remove("clickedPrio");
-    }
-    event.target.classList.add("clickedPrio");
+    document.getElementById(buttonId).classList.add(buttonId + "-bg-color");
+    document.getElementById("prio-" + prio).classList.add("prio-" + prio + "-span-color");
+
+    selectedPrio = prio;
+}
+
+function loadParticipants() {
+    let nameParticipant = document.getElementById(`selected-name`).innerHTML.split("");
+    console.log(nameParticipant);
+    document.querySelectorAll("#select-name option").forEach((elem) => {
+        elem.removeAttribute("selected");
+    });
 }
