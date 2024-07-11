79cf7296f49b19014deb0d32e3d650a09dc28f68
Offermanns <p.offermanns@D-Bogdol.local>
2024-07-11 13:41:25 +0200

    Active Link

--- build/js/main.js
+++ build/js/main.js
@@ -10,6 +10,9 @@ async function includeHTML() {
             element.innerHTML = 'Page not found';
         }
     }
+    if (typeof callback === 'function') {
+        callback();
+    }
 }
 
 function showUserMenu (){
@@ -48,3 +51,34 @@ function checkLogin(){
         window.location.href = 'login.html';
     }
 }
+
+function extractFilename(url) {
+    // Erstellen Sie ein URL-Objekt
+    const urlObj = new URL(url);
+    
+    // Extrahieren Sie den Pfad aus der URL
+    const pathname = urlObj.pathname;
+    
+    // Extrahieren Sie den Dateinamen aus dem Pfad
+    const filenameWithExtension = pathname.substring(pathname.lastIndexOf('/') + 1);
+    
+    // Entfernen Sie die Dateierweiterung
+    const filename = filenameWithExtension.split('.').slice(0, -1).join('.');
+    
+    return filename;
+  }
+  
+
+  function activeLink() {
+      setTimeout(() => {
+        const url = window.location.href;
+        const filename = extractFilename(url);
+        console.log(filename); // Gibt 'summary' aus
+      console.log(document.getElementById(filename));
+        document.getElementById(filename).classList.add('active');
+    }, 150);
+ 
+}
+
+activeLink();
+  
