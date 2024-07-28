293469e4c47f0de2afbe15f35a82bfa5de932d77
Oscar Ahlke <167767113+OAhlke1@users.noreply.github.com>
2024-07-28 07:43:02 +0200

    search function works again

---
+++ build/js/addTask.js
@@ -0,0 +1,358 @@
+let task = [];
+let participantsArray = [];
+let allTaskKeys = [];
+let name, sureName, lastName;
+let selectedPrio = "low";
+let allContactsObjects;
+let contactKeys = [];
+let allTaskObjects = [];
+let allSubtasksArray = [];
+let newTask;
+let selectContacts = document.querySelector('.contact-list');
+let subtaskInput = document.querySelector('#choose-subtasks');
+let categoryType = "medium";
+let taskAddedElem = document.querySelector('.task-added');
+const BASE_URL = "https://join-249-default-rtdb.europe-west1.firebasedatabase.app";
+const tasksURL = 'https://join-249-default-rtdb.europe-west1.firebasedatabase.app/tasks';
+
+
+/* This functions are mainly from Oscar Ahlke */
+async function getTasks() {
+    includeHTML();
+    let fetchedTasks = await fetch(tasksURL+'.json');
+    fetchedTasks = await fetchedTasks.json();
+    if(!fetchedTasks) {
+        getContacts();
+        return;
+    }
+    setTasksArray(fetchedTasks);
+    getContacts();
+}
+
+function setTasksArray(tasksJson) {
+    for(let [key, value] of Object.entries(tasksJson)) {
+        allTaskKeys.push(key);
+        allTaskObjects.push(value);
+    }
+}
+
+async function getContacts() {
+    allContactsObjects = await fetch(BASE_URL+'/contacts.json');
+    allContactsObjects = await allContactsObjects.json();
+    let contactsArray = [];
+    for(const [key, value] of Object.entries(allContactsObjects)) {
+        contactKeys.push(key);
+        contactsArray.push(value);
+    }
+    allContactsObjects = contactsArray;
+    sortContacts();
+}
+
+function showHideContactList(event) {
+    if(document.querySelector('.contact-list').classList.contains('disNone')) {
+        document.querySelector('.contact-list').classList.remove('disNone');
+        document.querySelector('.contacts .contacts-inner .triangle').classList.add('rotated');
+    }else {
+        document.querySelector('.contact-list').classList.add('disNone');
+        document.querySelector('.contacts .contacts-inner .triangle').classList.remove('rotated');
+    }
+}
+
+function showHideCategoriesList(event) {
+    if(document.querySelector('.categories-list').classList.contains('disNone')) {
+        document.querySelector('.categories-list').classList.remove('disNone');
+        document.querySelector('.categories .categories-inner .triangle').classList.add('rotated');
+    }else {
+        document.querySelector('.categories-list').classList.add('disNone');
+        document.querySelector('.categories .categories-inner .triangle').classList.remove('rotated');
+    }
+}
+
+function sortContacts() {
+    for (let i = 0; i < allContactsObjects.length - 1; i++) {
+        for (let j = i + 1; j < allContactsObjects.length; j++) {
+            if (allContactsObjects[i]["lastName"].toLowerCase() > allContactsObjects[j]["lastName"].toLowerCase()) {
+                puffer = allContactsObjects[i];
+                allContactsObjects[i] = allContactsObjects[j];
+                allContactsObjects[j] = puffer;
+            } else if (allContactsObjects[i]["lastName"].toLowerCase() === allContactsObjects[i]["lastName"].toLowerCase()) {
+                if (allContactsObjects[i]["sureName"].toLowerCase() > allContactsObjects[j]["sureName"].toLowerCase()) {
+                    puffer = allContactsObjects[i];
+                    allContactsObjects[i] = allContactsObjects[j];
+                    allContactsObjects[j] = puffer;
+                }
+            }
+        }   
+    }
+    renderSelectContacts();
+}
+
+function renderSelectContacts() {
+    selectContacts.innerHTML = /* HTML */ ``;
+    allContactsObjects.forEach((elem, i)=>{
+        selectContacts.innerHTML += /* HTML */ `<div class="flex flex-center contact" data-selectindex="${i}" onclick="selectContact(event)">
+        <div class="flex flex-center contact-left">
+            <div class="flex flex-center circle" style="background-color: ${allContactsObjects[i].color};"><p>${elem.sureName[0]}${elem.lastName[0]}</p></div>
+            <p>${elem.sureName} ${elem.lastName}</p>
+        </div>
+        <svg class="not-chosen" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect x="1" y="1" width="16" height="16" rx="3" stroke="#2A3647" stroke-width="2"/>
+        </svg>
+        <svg class="is-chosen" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M17 8V14C17 15.6569 15.6569 17 14 17H4C2.34315 17 1 15.6569 1 14V4C1 2.34315 2.34315 1 4 1H12" stroke="#2A3647" stroke-width="2" stroke-linecap="round" fill="white"/>
+            <path d="M5 9L9 13L17 1.5" stroke="#2A3647" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" fill="white"/>
+        </svg>
+    </div>`;
+    })
+}
+
+function renderChosenList() {
+    document.querySelector('.chosen-list').innerHTML = '';
+    participantsArray.forEach((elem, i)=>{
+        document.querySelector('.chosen-list').innerHTML += /* HTML */ `<li><div class="flex flex-center circle" onclick="removeParticipant(${i})" onmouseover="showName(${i})" onmouseleave="hideName(${i})" style="background-color: ${allContactsObjects[i].color};">
+            <p>${elem.sureName[0]}${elem.lastName[0]}</p>
+            <div class="name-block${i} name-block disNone"><p>${elem.sureName} ${elem.lastName}<br>Click icon to remove</p></div>
+        </div>
+    </li>`;
+    })
+    document.querySelector('.chosen-list').classList.remove('disNone');
+}
+
+function showName(i) {
+    document.querySelector(`.name-block${i}`).classList.remove('disNone');
+}
+
+function hideName(i) {
+    document.querySelector(`.name-block${i}`).classList.add('disNone');
+}
+
+function removeParticipant(i) {
+    participantsArray.splice(i, 1);
+    document.querySelectorAll('.contact.chosen')[i].classList.remove('chosen');
+    renderChosenList();
+}
+
+function selectContact(event) {
+    event.stopPropagation();
+    if(event.target.closest('.contact').classList.contains('chosen')) {
+        event.target.closest('.contact').classList.remove('chosen');
+    }else {
+        event.target.closest('.contact').classList.add('chosen');
+    }
+    getParticipants();
+}
+
+function getParticipants() {
+    participantsArray = [];
+    document.querySelectorAll('.contact.chosen').forEach((elem)=>{
+        participantsArray.push({
+            sureName: allContactsObjects[+elem.getAttribute('data-selectindex')].sureName,
+            lastName: allContactsObjects[+elem.getAttribute('data-selectindex')].lastName
+        })
+    })
+    renderChosenList();
+}
+
+function addTask(event) {
+    event.preventDefault();
+    newTask = {
+        taskTitle: document.getElementById("title-input").value,
+        taskDescrip: document.getElementById("textarea-input").value,
+        participants: participantsArray,
+        date: document.getElementById("date-input").value,
+        urgency: selectedPrio,
+        category: categoryType,
+        taskType: 'toDo',
+        deleted: 0
+    }
+    if(allSubtasksArray.length > 0) {
+        newTask.subTasks =  allSubtasksArray;
+    }
+    allTaskObjects.push(newTask);
+    postNewTask();
+}
+
+
+
+function choosePrio(event, prio) {
+    event.preventDefault();
+    if(event.target.closest('.choose-prio-button').classList.contains(`prio-${prio}-button-bg-color`)) {
+        event.target.closest('.choose-prio-button').classList.remove(`prio-${prio}-button-bg-color`);
+        selectedPrio = "low";
+        return;
+    }
+    document.getElementById("prio-high-button").classList.remove("prio-high-button-bg-color");
+    document.getElementById("prio-medium-button").classList.remove("prio-medium-button-bg-color");
+    document.getElementById("prio-low-button").classList.remove("prio-low-button-bg-color");
+    event.target.closest('.choose-prio-button').classList.add(`prio-${prio}-button-bg-color`);
+    selectedPrio = prio;
+}
+
+async function postNewTask(event) {
+    console.log(allTaskObjects.length);
+    let response = await fetch(BASE_URL+"/tasks.json", {
+        method: 'PUT',
+        header: {'Content-Type': 'application/json'},
+        body: JSON.stringify(allTaskObjects)
+    });
+    fadeInTaskAdded();
+}
+
+function showCrossTic() {
+    document.querySelector('.subtask-input .add').classList.add('disNone');
+    document.querySelector('.cross-tic').classList.remove('disNone');
+}
+
+function hideCrossTic() {
+    document.querySelector('.subtask-input .add').classList.remove('disNone');
+    document.querySelector('.cross-tic').classList.add('disNone');
+}
+
+document.querySelector('.clear-subtask-input').addEventListener('click', (event)=>{
+    subtaskInput.removeEventListener('focusout', hideCrossTic);
+    subtaskInput.focus();
+    subtaskInput.value = '';
+});
+
+function addSubtask() {
+    if(subtaskInput.value === "") {
+        hideCrossTic();
+        return;
+    }else {
+        if(checkIfSubtaskExists()) {
+            subtaskInput.value = '';
+            alert('Subtask already exists');
+        }else {
+            allSubtasksArray.push({subTaskDone: 0, subTaskTitle: subtaskInput.value})
+            renderSubtaskList();
+            subtaskInput.value = '';
+        }
+        hideCrossTic();
+    }
+}
+
+function checkIfSubtaskExists() {
+    for(let i=0; i<allSubtasksArray.length; i++) {
+        if(allSubtasksArray[i].subTaskTitle.toLowerCase() === subtaskInput.value.toLowerCase()) {
+            return true;
+        }else if(allSubtasksArray[i].subTaskTitle.toLowerCase() != subtaskInput.value.toLowerCase()) {
+            if(i+1 === allSubtasksArray.length) {
+                return false;
+            }
+        }
+    }
+}
+
+function renderSubtaskList() {
+    document.querySelector('.subtask-list').innerHTML = '';
+    allSubtasksArray.forEach((elem, i)=>{
+        document.querySelector('.subtask-list').innerHTML += /* HTML */ `<li id="subtask-li-${i}" class="flex flex-center" style="column-gap: 12px;" onmouseover="fadeInPenBin(${i})" onmouseleave="fadeOutPenBin(${i})">
+            <p class="subtask-title-p-${i}">${elem.subTaskTitle}</p>
+            <div class="pen-bin-subtask flex flex-center" id="pen-bin-subtask-${i}">
+                <img src="../../assets/img/pen.svg" alt="" onclick="editSubtask(${i})">
+                <img src="../../assets/img/bin.svg" alt="" onclick="removeSubtask(${i})">
+            </div>
+            <div class="edit-subtask flex flex-center disNone" id="edit-subtask-${i}" style="justify-content: space-between;">
+                <input type="text" id="edit-subtask-input-${i}" value="${elem.subTaskTitle}">
+                <div class="bin-check flex flex-center">
+                    <img src="./assets/img/bin.svg" alt="" onclick="removeSubtask(${i})">
+                    <img src="./assets/img/check-icon-black.svg" alt="" onclick="changeSubtask(${i})">
+                </div>
+            </div>
+        </li>`
+    })
+    document.querySelector('.subtask-list').classList.remove('disNone');
+}
+
+function fadeInPenBin(i) {
+    let liTag = document.querySelector(`#subtask-li-${i}`);
+    liTag.classList.remove('fade-out-pen-bin');
+    liTag.classList.add('fade-in-pen-bin');
+}
+
+function fadeOutPenBin(i) {
+    let liTag = document.querySelector(`#subtask-li-${i}`);
+    liTag.classList.add('fade-out-pen-bin');
+    liTag.classList.remove('fade-in-pen-bin');
+}
+
+function editSubtask(i) {
+    let editElem = document.querySelector(`#edit-subtask-${i}`);
+    document.querySelector(`.subtask-title-p-${i}`).classList.add('disNone');
+    document.querySelector(`#pen-bin-subtask-${i}`).classList.add('disNone');
+    editElem.classList.remove('disNone');
+    editElem.querySelector('input').focus();
+}
+
+function changeSubtask(i) {
+    allSubtasksArray[i].subTaskTitle = document.querySelector(`#edit-subtask-input-${i}`).value;
+    renderSubtaskList();
+    document.querySelector(`#edit-subtask-${i}`).classList.add('disNone');
+}
+
+function removeSubtask(i) {
+    allSubtasksArray.splice(i, 1);
+    renderSubtaskList();
+}
+
+function setCategory(event) {
+    event.preventDefault();
+    document.querySelector('.category-name').innerHTML = event.target.innerHTML;
+    categoryType = event.target.closest('.category').querySelector('p').innerHTML;
+}
+
+/**
+ * 
+ * This function clears and resets all input-fields and variables belonging to the editing elements of the add-task-overlay.
+ */
+function clearForm(event) {
+    event.preventDefault();
+    document.querySelector('#title-input').value = "";
+    document.querySelector('#textarea-input').value = "";
+    document.querySelector('.contacts-inner p').value = "";
+    document.querySelector('.chosen-list').innerHTML = "";
+    document.querySelector('#date-input').value = "";
+    resetUrgency();
+    document.querySelector('.category-name').innerHTML = "Select task category";
+    document.querySelector('#choose-subtasks').value = "";
+    document.querySelector('.subtask-list').innerHTML = "";
+    document.querySelectorAll('.contact-list .contact.chosen').forEach((elem)=>{elem.classList.remove('chosen');});
+    document.querySelector('.cross-tic').classList.add('disNone');
+    allSubtasksArray = [];
+}
+
+/**
+ * 
+ * This function resets the urgency of the new task.
+ */
+function resetUrgency() {
+    document.querySelectorAll('.choose-prio-button')[0].classList.remove('prio-high-button-bg-color');
+    document.querySelectorAll('.choose-prio-button')[1].classList.remove('prio-medium-button-bg-color');
+    document.querySelectorAll('.choose-prio-button')[2].classList.remove('prio-low-button-bg-color');
+    document.querySelectorAll('.choose-prio-button')[1].classList.add('prio-medium-button-bg-color');
+    newUrgency = "medium";
+}
+
+/**
+ * 
+ *  @function fadeInTaskAdded adds the class 'added' to @var taskAddedElem to fade it in via CSS.
+ */
+function fadeInTaskAdded() {
+    console.log('Hi!');
+    taskAddedElem.classList.remove('disNone');
+    taskAddedElem.classList.remove('not-added');
+    taskAddedElem.classList.add('added');
+    setTimeout(fadeOutTaskAdded, 1000);
+}
+
+/**
+ * 
+ *  @function fadeOutTaskAdded removes the class 'added' and adds the class 'not-added' to @var taskAddedElem to fade it out via CSS.
+ */
+function fadeOutTaskAdded() {
+    taskAddedElem.classList.remove('added');
+    taskAddedElem.classList.add('not-added');
+    setTimeout(()=>{
+        taskAddedElem.classList.add('disNone');
+    }, 700);
+}
---
+++ build/js/contact.js
@@ -0,0 +1,324 @@
+const BASE_URL =
+  "https://join-249-default-rtdb.europe-west1.firebasedatabase.app";
+
+let contacts = [];
+let puffer;
+let newChar;
+let letterBlock = ``;
+let contactsString = "";
+let contactList = document.querySelector(".contactList");
+let contactsIndex = 0;
+let showContacts = document.querySelector("#showContact");
+let toggleEditContact = true;
+let toggleAddContact = true;
+let toggleInfoContact = true;
+let contactKeys = [];
+let sureLastName;
+let nameSuffix = false;
+let newContact;
+let q = 0;
+let presentlyIndexContacts;
+let information = document.querySelector(".informationPopUp");
+let color;
+
+function init() {
+  includeHTML();
+  getContacts();
+}
+
+async function getContacts() {
+  contacts = await fetch(BASE_URL + "/contacts.json");
+  contacts = await contacts.json();
+  setContactsAsArray();
+  sorter();
+}
+
+function setContactsAsArray() {
+  let contacsKeysArray = [];
+  for (let [key, value] of Object.entries(contacts)) {
+    contacsKeysArray.push([key, value]);
+  }
+  contacts = contacsKeysArray;
+}
+
+function sorter() {
+  contacts.sort((a, b) => {
+    if (a[1].lastName > b[1].lastName) {
+      return 1;
+    } else if (a[1].lastName < b[1].lastName) {
+      return -1;
+    } else {
+      return a[1].sureName > b[1].sureName
+        ? 1
+        : a[1].sureName < b[1].sureName
+        ? -1
+        : 0;
+    }
+  });
+
+  renderIntoLetterBox();
+}
+
+function renderIntoLetterBox() {
+  if (contacts.length === 0) {
+    showContacts.innerHTML = "";
+  }
+  newChar = contacts[contactsIndex][1]["lastName"][0];
+  getContactsHtml();
+  if (contactsIndex < contacts.length) {
+    letterBlock += `<h3 class="sort"> ${newChar}</h3>${contactsString}`;
+    contactsString = "";
+  }
+  if (contactsIndex === contacts.length) {
+    showContacts.innerHTML = letterBlock;
+    return;
+  }
+  renderIntoLetterBox();
+}
+
+function getContactsHtml() {
+  for (contactsIndex = q; q < contacts.length; q++) {
+    if (contactsIndex == contacts.length) {
+      return;
+    }
+    contactsIndex = q;
+    if (newChar != contacts[q][1].lastName[0]) {
+      return;
+    }
+
+    contactsString += contactHTML(contactsIndex, q);
+  }
+}
+
+function contactHTML(contactsIndex, q) {
+  getRandomColor();
+  return ` <div
+      class="flex contact"
+      onclick="clickContact(event)"
+      data-contactIndex="${contactsIndex}">
+      <div id="profileImage" class="flex-center" style="background-color: ${color};" >
+        ${contacts[q][1].sureName[0]}${contacts[q][1].lastName[0]}
+      </div>
+      <div class="gap"> 
+        <li>${contacts[q][1].sureName} ${contacts[q][1].lastName}</li>
+        <span>${contacts[q][1].email}</span>
+      </div>
+    </div>`;
+}
+
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  color = "#0";
+  for (let i = 0; i < 5; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function clickContact(event) {
+  if (window.innerWidth < 900) {
+    showHideContactNames();
+  }
+  event.stopPropagation();
+  presentlyIndexContacts = +event.target
+    .closest(".contact")
+    .getAttribute("data-contactIndex");
+
+  information.innerHTML = clickContactHTML(presentlyIndexContacts);
+}
+
+function showHideContactNames() {
+if(contactList.classList.contains("d-none")){
+  contactList.classList.remove("d-none");
+}else if(!contactList.classList.contains("d-none")){
+  contactList.classList.add("d-none");
+  }
+}
+
+function hideContact(){
+let back = document.querySelector(".backArrow");
+  contactList.classList.remove("d-none");
+back.classList.add("d-none");
+}
+
+function clickContactHTML(index) {
+  getRandomColor();
+  return ` <div class="flex showContactName">
+    
+      <div id="profileImage" class="flex-center bigSize"  style="background-color: ${color};">
+        ${contacts[index][1]["sureName"][0]}${contacts[index][1]["lastName"][0]}
+      </div>
+      <div>
+        <span>
+          ${contacts[index][1]["sureName"]}
+          ${contacts[index][1]["lastName"]}
+        </span>
+        <div>
+          <img
+            src="./assets/img/editContacts.png"
+            alt=""
+            onclick="startingValueEditContact(${index})"
+          />
+          <img
+            src="./assets/img/DeleteContact.png"
+            alt=""
+            onclick="deleteContact(${presentlyIndexContacts})"
+          />
+        </div>
+      </div>
+      <img class="backArrow" onclick="hideContact()" src="./assets/img/backArrow.png" alt="">
+    </div>
+     
+    <div class="contactInformations">
+      <p>Contact Information</p>
+      <div>
+        <h5>Email</h5>
+        <span> ${contacts[index][1]["email"]}</span>
+        <h5>Phone</h5>
+        <span> ${contacts[index][1]["number"]}</span>
+      </div>
+    </div>`;
+}
+
+function startingValueEditContact(index) {
+  let name = document.querySelector(".inputEditName");
+  let email = document.querySelector(".inputEditEmail");
+  let number = document.querySelector(".inputEditNumber");
+  let letters = document.querySelector(".editContactImg");
+
+  name.value =
+    contacts[index][1]["sureName"] + " " + contacts[index][1]["lastName"];
+  email.value = contacts[index][1]["email"];
+  number.value = contacts[index][1]["number"];
+  letters.innerHTML =
+    contacts[index][1]["sureName"][0] + contacts[index][1]["lastName"][0];
+
+  editContactToggle();
+}
+
+function editContact() {
+  let name = document.querySelector(".inputEditName");
+  let email = document.querySelector(".inputEditEmail");
+  let number = document.querySelector(".inputEditNumber");
+  sureLastName = document.querySelector(".inputEditName").value.split(" ");
+
+  editContactToggle();
+  deleteContact(presentlyIndexContacts);
+  setTimeout(function () {
+    createContact(email, number, name, sureLastName);
+  }, 100);
+}
+
+function deleteContact(index) {
+  deleteData("/contacts/" + contacts[index][0]);
+  contacts.splice(index, 1);
+  letterBlock = "";
+  contactsIndex = 0;
+  q = 0;
+  information.innerHTML = "";
+  renderIntoLetterBox();
+}
+
+async function deleteData(path = "") {
+  let response = await fetch(BASE_URL + path + ".json", {
+    method: "DELETE",
+  });
+  return (responseToJson = await response.json());
+}
+
+async function postData(path = "", data = {}) {
+  let response = await fetch(BASE_URL + path + ".json", {
+    method: "POST",
+    header: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  return (responseToJson = await response.json());
+}
+
+function createContactValue() {
+  let email = document.querySelector(".inputEmail");
+  let number = document.querySelector(".inputNumber");
+  let name = document.querySelector(".inputName");
+  sureLastName = document.querySelector(".inputName").value.split(" ");
+  createContact(email, number, name, sureLastName);
+  addContactToggle();
+}
+
+function createContact(email, number, name, sureLastName) {
+  if (sureLastName.length == 1) {
+    sureLastName.push("");
+  }
+  convertNames();
+  newContact = {
+    sureName: sureLastName[0],
+    lastName: sureLastName[1],
+    email: email.value,
+    number: number.value,
+    color: `#${Math.round(255 * Math.random()).toString(16)}${Math.round(
+      255 * Math.random()
+    ).toString(16)}${Math.round(255 * Math.random()).toString(16)}`,
+    contactId: Math.random(),
+  };
+  contacts.push([contacts.length + 1, newContact]);
+  resetValue(email, number, name);
+  postData("/contacts", newContact);
+  getContacts();
+}
+
+function resetValue(email, number, name) {
+  email.value = "";
+  number.value = "";
+  name.value = "";
+  showContacts.innerHTML = "";
+  contactsIndex = 0;
+  q = 0;
+  letterBlock = "";
+  newChar = "A";
+}
+
+function convertNames() {
+  let rest;
+
+  for (let i = 0; i < sureLastName.length; i++) {
+    if (sureLastName[i] === "von" || sureLastName[i] === "zu") {
+      nameSuffix = true;
+      continue;
+    }
+    rest = sureLastName[i].slice(1, sureLastName[i].length);
+    sureLastName[i] = sureLastName[i][0].toUpperCase() + rest;
+  }
+
+  hasNameSuffix();
+}
+
+function hasNameSuffix() {
+  if (!nameSuffix) {
+    return;
+  }
+
+  sureLastName = [sureLastName[0], sureLastName[1] + " " + sureLastName[2]];
+}
+
+function editContactToggle() {
+  let editContact = document.querySelector(".overlayEdit-parent");
+  if (toggleEditContact) {
+    editContact.classList.remove("d-none");
+    toggleEditContact = false;
+  } else {
+    editContact.classList.add("d-none");
+    toggleEditContact = true;
+  }
+}
+
+function addContactToggle() {
+  let addContact = document.querySelector(".overlay-parent");
+  if (toggleAddContact) {
+    addContact.classList.remove("d-none");
+    toggleAddContact = false;
+  } else {
+    addContact.classList.add("d-none");
+    toggleAddContact = true;
+  }
+}
